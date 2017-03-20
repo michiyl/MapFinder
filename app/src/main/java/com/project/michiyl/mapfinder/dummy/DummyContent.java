bd76288143f7f7491a6e3a007ce7e12ea900e44e
michiyl <mihenke@web.de>
2017-03-20 15:53:43 +0100

    Changed: - reading descriptions works!

--- app/src/main/java/com/project/michiyl/mapfinder/dummy/DummyContent.java
+++ app/src/main/java/com/project/michiyl/mapfinder/dummy/DummyContent.java
@@ -4,12 +4,16 @@ import android.graphics.Path;
 import android.provider.MediaStore;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Helper class for providing sample content for user interfaces created by
@@ -61,12 +65,52 @@ public class DummyContent {
     }
 
     private static String makeDetails(int position) {
+        /*
         StringBuilder builder = new StringBuilder();
         builder.append("Details about Item: ").append(position);
         for (int i = 0; i < position; i++) {
             builder.append("\nMore details information here.");
         }
         return builder.toString();
+        */
+
+        int countOfDirectories = MapContent.MapItem.myMapDirectory.listFiles().length;
+        if(countOfDirectories <= 0) {
+            Log.e("michiyl", "ERROR --- NO DIRECTORY!");
+            return "";
+        }
+
+        File theDirectory = MapContent.MapItem.myMapDirectory;
+        String[] filenames = theDirectory.list();
+        String sb = "";
+        File myFile = null;
+
+        // check which one we want to read from this time
+        // FULL path necessary!
+        myFile = new File(theDirectory.getAbsolutePath() + "/" + filenames[position-1], "/description.txt");
+
+        Log.d("michiyl", "myFile Description: " + myFile.toString());
+        Log.d("michiyl", "exists myFile Description: " + myFile.exists());
+        Log.d("michiyl", "canRead myFile Description: " + myFile.canRead());
+
+        try (Scanner br = new Scanner(new FileReader(myFile))) {
+            // read just the lines
+            if(br.hasNext())
+                while( br.hasNext()) {
+                    sb += br.nextLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e("michiyl", "FileNotFoundException!", e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("michiyl", "IOException!", e);
+        }
+
+        Log.d("michiyl", "sb: " + sb);
+        return sb.toString();
+
     }
 
     /**
